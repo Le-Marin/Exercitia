7f4de83ef8e68e0019a4040792f5fda71a0636a0
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2023-08-26 10:01:59 +0300

    Add files via upload

---
+++ src/llpsi/ultra/ultra-07-2gsfds8.js
@@ -0,0 +1,158 @@
+$_GET({
+  title: 'LLPSI: Ultra 7.2',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Определить падежную функцию выделенных слов.</b></div>
+      <div><br></div>
+      <div><i><div class="area">субъект, подлежащее</div> <div class="area">предикатив (именная часть сказуемого)</div> <div class="area">принадлежность, отношение</div> <div class="area">адресат, получатель</div> <div class="area">объект действия, прямое дополнение</div> <div class="area">обращение</div></i></div>
+      <div><br></div>
+      <div>1. Юлий — <b>римлянин</b>. <div class="area" data-key="предикатив"></div></div>
+      <div>2. <b>Юлий</b> имеет большую семью. <div class="area" data-key="субъект | подлежащее"></div></div>
+      <div>3. Семья <b>Юлия</b> большая. <div class="area" data-key="принадлежность | отношение"></div></div>
+      <div>4. Эмилия дарит <b>Юлию</b> поцелуй. <div class="area" data-key="адресат | получатель"></div></div>
+      <div>5. <b>Юлия</b> несут рабы. <div class="area" data-key="объект действия | прямое дополнение"></div></div>
+      <div>6. Послушай меня, <b>Юлий</b>! <div class="area" data-key="обращение"></div></div>
+      <div><br></div>
+      <div>7. <b>Mārcus</b> pulsat Quīntum. <div class="area" data-key="субъект | подлежащее"></div></div>
+      <div>8. Nōn <b>fīlius Cornēliī</b> est Mārcus. <div class="area" data-key="предикатив"></div></div>
+      <div>9. Pater <b>Mārcī</b> est Iūlius. <div class="area" data-key="принадлежность | отношение"></div></div>
+      <div>10. Venī, <b>Mārce</b>! <div class="area" data-key="обращение"></div></div>
+      <div>11. Iūlius <b>Mārcō</b> māla piraque dat. <div class="area" data-key="адресат | получатель"></div></div>
+      <div>12. <b>Mārcum</b>ne vocat māter? <div class="area" data-key="объект действия | прямое дополнение"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>2. Дать форму генетива.</b></div>
+      <div><br></div>
+      <div>ancill<b>a</b> → <div class="area" data-key="ancillae"></div></div>
+      <div>īnsul<b>a</b> → <div class="area" data-key="īnsulae"></div></div>
+      <div>Rōm<b>a</b>  → <div class="area" data-key="Rōmae"></div></div>
+      <div>serv<b>us</b>  → <div class="area" data-key="servī"></div></div>
+      <div>liber  → <div class="area" data-key="librī"></div></div>
+      <div>vir  → <div class="area" data-key="virī"></div></div>
+      <div>oppid<b>um</b> → <div class="area" data-key="oppidī"></div></div>
+      <div>māl<b>um</b> → <div class="area" data-key="mālī"></div></div>
+      <div>ōsti<b>um</b> → <div class="area" data-key="ōstiī"></div></div>
+      <div><br></div>
+      <div>fēmin<b>ae</b> → <div class="area" data-key="fēminārum"></div></div>
+      <div>amīc<b>ae</b> → <div class="area" data-key="amīcārum"></div></div>
+      <div>vīll<b>ae</b> → <div class="area" data-key="vīllārum"></div></div>
+      <div>domin<b>ī</b> → <div class="area" data-key="dominōrum"></div></div>
+      <div>līber<b>ī</b> → <div class="area" data-key="līberōrum"></div></div>
+      <div>vir<b>ī</b> → <div class="area" data-key="virōrum"></div></div>
+      <div>pir<b>a</b> → <div class="area" data-key="pirōrum"></div></div>
+      <div>ōscul<b>a</b> → <div class="area" data-key="ōsculōrum"></div></div>
+      <div>specul<b>a</b> → <div class="area" data-key="speculōrum"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>3. Дать форму аккузатива.</b></div>
+      <div><br></div>
+      <div>ancill<b>a</b> → <div class="area" data-key="ancillam"></div></div>
+      <div>īnsul<b>a</b> → <div class="area" data-key="īnsulam"></div></div>
+      <div>Rōm<b>a</b>  → <div class="area" data-key="Rōmam"></div></div>
+      <div>serv<b>us</b>  → <div class="area" data-key="servum"></div></div>
+      <div>liber  → <div class="area" data-key="librum"></div></div>
+      <div>vir  → <div class="area" data-key="virum"></div></div>
+      <div>oppid<b>um</b> → <div class="area" data-key="oppidum | ="></div></div>
+      <div>māl<b>um</b> → <div class="area" data-key="mālum | ="></div></div>
+      <div>ōsti<b>um</b> → <div class="area" data-key="ōstium | ="></div></div>
+      <div><br></div>
+      <div>fēmin<b>ae</b> → <div class="area" data-key="fēminās"></div></div>
+      <div>amīc<b>ae</b> → <div class="area" data-key="amīcās"></div></div>
+      <div>vīll<b>ae</b> → <div class="area" data-key="vīllās"></div></div>
+      <div>domin<b>ī</b> → <div class="area" data-key="dominōs"></div></div>
+      <div>līber<b>ī</b> → <div class="area" data-key="līberōs"></div></div>
+      <div>vir<b>ī</b> → <div class="area" data-key="virōs"></div></div>
+      <div>pir<b>a</b> → <div class="area" data-key="pira | ="></div></div>
+      <div>ōscul<b>a</b> → <div class="area" data-key="ōscula | ="></div></div>
+      <div>specul<b>a</b> → <div class="area" data-key="specula | ="></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>4. Дать форму аблятива.</b></div>
+      <div><br></div>
+      <div>ancill<b>a</b> → <div class="area" data-key="ancillā"></div></div>
+      <div>īnsul<b>a</b> → <div class="area" data-key="īnsulā"></div></div>
+      <div>Rōm<b>a</b>  → <div class="area" data-key="Rōmā"></div></div>
+      <div>serv<b>us</b>  → <div class="area" data-key="servō"></div></div>
+      <div>liber  → <div class="area" data-key="librō"></div></div>
+      <div>vir  → <div class="area" data-key="virō"></div></div>
+      <div>oppid<b>um</b> → <div class="area" data-key="oppidō"></div></div>
+      <div>māl<b>um</b> → <div class="area" data-key="mālō"></div></div>
+      <div>ōsti<b>um</b> → <div class="area" data-key="ōstiō"></div></div>
+      <div><br></div>
+      <div>fēmin<b>ae</b> → <div class="area" data-key="fēminīs"></div></div>
+      <div>amīc<b>ae</b> → <div class="area" data-key="amīcīs"></div></div>
+      <div>vīll<b>ae</b> → <div class="area" data-key="vīllīs"></div></div>
+      <div>domin<b>ī</b> → <div class="area" data-key="dominīs"></div></div>
+      <div>līber<b>ī</b> → <div class="area" data-key="līberīs"></div></div>
+      <div>vir<b>ī</b> → <div class="area" data-key="virīs"></div></div>
+      <div>pir<b>a</b> → <div class="area" data-key="pirīs"></div></div>
+      <div>ōscul<b>a</b> → <div class="area" data-key="ōsculīs"></div></div>
+      <div>specul<b>a</b> → <div class="area" data-key="speculīs"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>5. Дать форму датива.</b></div>
+      <div><br></div>
+      <div>ancill<b>a</b> → <div class="area" data-key="ancillae"></div></div>
+      <div>īnsul<b>a</b> → <div class="area" data-key="īnsulae"></div></div>
+      <div>Rōm<b>a</b>  → <div class="area" data-key="Rōmae"></div></div>
+      <div>serv<b>us</b>  → <div class="area" data-key="servō"></div></div>
+      <div>liber  → <div class="area" data-key="librō"></div></div>
+      <div>vir  → <div class="area" data-key="virō"></div></div>
+      <div>oppid<b>um</b> → <div class="area" data-key="oppidō"></div></div>
+      <div>māl<b>um</b> → <div class="area" data-key="mālō"></div></div>
+      <div>ōsti<b>um</b> → <div class="area" data-key="ōstiō"></div></div>
+      <div><br></div>
+      <div>fēmin<b>ae</b> → <div class="area" data-key="fēminīs"></div></div>
+      <div>amīc<b>ae</b> → <div class="area" data-key="amīcīs"></div></div>
+      <div>vīll<b>ae</b> → <div class="area" data-key="vīllīs"></div></div>
+      <div>domin<b>ī</b> → <div class="area" data-key="dominīs"></div></div>
+      <div>līber<b>ī</b> → <div class="area" data-key="līberīs"></div></div>
+      <div>vir<b>ī</b> → <div class="area" data-key="virīs"></div></div>
+      <div>pir<b>a</b> → <div class="area" data-key="pirīs"></div></div>
+      <div>ōscul<b>a</b> → <div class="area" data-key="ōsculīs"></div></div>
+      <div>specul<b>a</b> → <div class="area" data-key="speculīs"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>6. Дать нужную форму после предлога.</b></div>
+      <div><br></div>
+      <div>Graec<b>ī</b> → apud <div class="area" data-key="Graecōs"></div></div>
+      <div>vīll<b>a</b> → ē <div class="area" data-key="vīllā"></div></div>
+      <div>mūr<b>ī</b> → circum <div class="area" data-key="mūrōs"></div></div>
+      <div>port<b>a</b> → per <div class="area" data-key="portam"></div></div>
+      <div>litter<b>ae</b> → ante <div class="area" data-key="litterās"></div></div>
+      <div>Tūscul<b>um</b> → prope <div class="area" data-key="Tūsculum"></div></div>
+      <div>fluvi<b>ī</b> → sine <div class="area" data-key="fluviīs"></div></div>
+      <div>amīc<b>us</b> → cum <div class="area" data-key="amīcō"></div></div>
+      <div>oppid<b>um</b> → ex <div class="area" data-key="oppidō"></div></div>
+      <div>ātri<b>um</b> → ab <div class="area" data-key="ātriō"></div></div>
+      <div>ego → post <div class="area" data-key="mē"></div></div>
+      <div>tū → ad <div class="area" data-key="tē"></div></div>
+      <div>īnsul<b>ae</b> → inter <div class="area" data-key="īnsulās"></div></div>
+      <div>aqu<b>a</b> → <i>(ubi?)</i> in <div class="area" data-key="aquā"></div></div>
+      <div>hort<b>us</b> → <i>(quō?)</i> in <div class="area" data-key="hortō"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>7. Задать вопросы к выделенным словам.</b></div>
+      <div><br></div>
+      <div><div><i>Exemplum</i>:<span style="white-space: pre;">	</span><b>Iūlius</b> Tūsculī est. <div class="area">quis?</div></div>
+      <div><span style="white-space: pre;">	</span><span style="white-space: pre;">	</span><span style="white-space: pre;">	</span>Iūlius <b>Tūsculī</b> est. <div class="area">ubi?</div></div></div>
+      <div><br></div>
+      <div>1. <b>Cornēlius </b>nōn ambulat, sed equō vehitur. <div class="area" data-key="quis?"></div></div>
+      <div>2. Aemilia <b>fīlium improbum</b> verberat. <div class="area" data-key="quem?"></div></div>
+      <div>3. Puerī <b>duōs saccōs</b> ante vīllam vident. <div class="area" data-key="quid?"></div></div>
+      <div>4. Aemilia <b>virō suō</b> ōsculum dat. <div class="area" data-key="cui?"></div></div>
+      <div>5. Iūlius <b>decem</b> nummōs habet in sacculō suō. <div class="area" data-key="quot?"></div></div>
+      <div>6. Mārcus et Quīntus <b>fīliī Iūliī</b> sunt. <div class="area" data-key="quī?"></div></div>
+      <div>7. Iūlia est fīlia <b>Iūliī</b>. <div class="area" data-key="cuius?"></div></div>
+      <div>8. Lȳdia <b>Rōmae</b> habitat. <div class="area" data-key="ubi?"></div></div>
+      <div>9. Mēdus <b>Rōmam </b>it. <div class="area" data-key="quō?"></div></div>
+      <div>10. Cornēlius <b>Rōmā </b>venit. <div class="area" data-key="unde?"></div></div>
+    </div>
+  `
+});
