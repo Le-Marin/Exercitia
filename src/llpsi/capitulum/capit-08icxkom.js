65f9c998add8a44b2cc19737183a0f2f5fb5e446
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2023-08-26 10:07:26 +0300

    Add files via upload

---
+++ src/llpsi/capitulum/capit-08icxkom.js
@@ -0,0 +1,129 @@
+$_GET({
+  title: 'LLPSI: Capitulum 8',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I<br></div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div>
+      <div><i><div class="area">abīre</div> <div class="area">accipere</div> <div class="area">alius</div> <div class="area">ānulus</div> <div class="area">aspicere</div> <div class="area">collum</div> <div class="area">cōnsistere</div> <div class="area">digitus</div> <div class="area">emere</div> <div class="area">gemma</div> <div class="area">margarīta</div> <div class="area">ōrnāmentum</div> <div class="area">ōrnāre</div> <div class="area">pecūniōsus</div> <div class="area">quī</div> <div class="area">quae</div> <div class="area">taberna</div> <div class="area">tabernārius</div> <div class="area">vēndere</div></i></div>
+      <div><br></div>
+      <div>1. In <div class="area" data-key="tabernā"></div> Albīnī sunt multae <div class="area" data-key="gemmae | margarītae"></div> et <div class="area" data-key="margarītae | gemmae"></div> et alia <div class="area" data-key="ōrnāmenta"></div>.</div>
+      <div>2. Albīnus est tabernārius quī ōrnāmenta <div class="area" data-key="vēndit"></div>.</div>
+      <div>3. <div class="area" data-key="aliī"></div> tabernāriī māla et pira vēndunt.</div>
+      <div>4. <div class="area" data-key="quī"></div> tabernam habet <div class="area" data-key="tabernārius"></div> est.</div>
+      <div>5. Multae fēminae ante tabernam Albīnī <div class="area" data-key="cōnsistunt"></div>.</div>
+      <div>6. Eae quae pecūniam habent ōrnāmenta <div class="area" data-key="emunt"></div>.</div>
+      <div>7. <div class="area" data-key="quae"></div> pecūniam nōn habent ōrnāmenta <div class="area" data-key="aspiciunt"></div> et rūrsus <div class="area" data-key="abeunt"></div>.</div>
+      <div>8. Aemilia multa ōrnāmenta ā virō suō <div class="area" data-key="accipit"></div>.</div>
+      <div>9. Aemilia <div class="area" data-key="ānulun"></div> in digitō et margarītās in <div class="area" data-key="collō"></div> habet.</div>
+      <div>10. Margarītae collum Aemiliae <div class="area" data-key="ōrnant"></div>.</div>
+      <div>11. <div class="area" data-key="digitus"></div> Aemiliae ānulō ōrnātur.</div>
+      <div>12. Quī magnam pecūniam habet <div class="area" data-key="pecūniōsus"></div> est.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 3</b></div>
+      <div><br></div>
+      <div><i>Exemplum:</i><span style="white-space: pre;">	</span>Iūlia Iūliō rosam <b>dat</b>. = Iūlius rosam <b>accipit</b> ā Iūliā.</div>
+      <div><br></div>
+      <div>1. Iūlius Dāvō nummum dat. = Dāvus <div class="area" data-key="nummum accipit ā Iūliō"></div>.</div>
+      <div>2. Aemilia ānulum accipit ā Iūliō. = <div class="area" data-key="Iūlius Aemiliae ānulum dat"></div>.</div>
+      <div>3. Virī fēminīs ōrnāmenta dant. = <div class="area" data-key="Fēminae ōrnāmenta accipiunt ā virīs"></div>.</div>
+      <div>4. Servus mālum accipit ā dominō. = <div class="area" data-key="Dominus servō mālum dat"></div>.</div>
+      <div>5. Mēdus Lȳdiae rosam dat. = <div class="area" data-key="Lȳdia rosam accipit ā Mēdō"></div>.</div>
+      <div>6. Servī nummōs accipiunt ā dominō. = <div class="area" data-key="Dominus servīs nummōs dat"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 4</b></div>
+      <div><br></div>
+      <div><i><div class="area">ad-venit, ad-veniunt</div> <div class="area">ad-it, ad-eunt</div> <div class="area">ab-it, ab-eunt</div> <div class="area">ex-it, ex-eunt</div> <div class="area">ad-est, ad-sunt</div> <div class="area">ab-est, ab-sunt</div> <div class="area">in-est, īn-sunt</div></i></div>
+      <div><br></div>
+      <div>1. Iūlius ad vīllam <div class="area" data-key="advenit"></div>; quattuor servī cum eō <div class="area" data-key="adveniunt"></div>.</div>
+      <div>2. Aemilia ē peristȳlō <div class="area" data-key="exit"></div> et in ātrium intrat.</div>
+      <div>3. Aemilia ad virum suum <div class="area" data-key="adit"></div>.</div>
+      <div>4. Iūlia ab Aemiliā <div class="area" data-key="abit"></div>; etiam puerī <div class="area" data-key="abeunt"></div>.</div>
+      <div>5. Servī in ātrium intrant et rūrsus <div class="area" data-key="exeunt"></div>.</div>
+      <div>6. Multae fēminae ad tabernam <div class="area" data-key="adeunt"></div> et rūrsus <div class="area" data-key="abeunt"></div>.</div>
+      <div>7. Quid <div class="area" data-key="inest"></div> in sacculō? In sacculō nummī <div class="area" data-key="īnsunt"></div>.</div>
+      <div>8. Quot servī sunt in ātriō? Duo servī <div class="area" data-key="adsunt"></div>, cēterī <div class="area" data-key="absunt"></div>.</div>
+      <div>9. Mēdus nōn <div class="area" data-key="adest"></div>: Mēdus <div class="area" data-key="abest"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II<br></div>
+      <div><br></div>
+      <div><b>Exercitium 5</b></div>
+      <div><br></div>
+      <div><i><div class="area">num</div> <div class="area">nōnne</div></i></div>
+      <div><br></div>
+      <div>1. Iūlia: “<div class="area" data-key="num"></div> nāsus meus foedus est?”</div>
+      <div>2. Syra: “Quid? <div class="area" data-key="nōnne"></div> fōrmōsus est nāsus meus?”</div>
+      <div>3. Iūlius: “Ubi est Iūlia? <div class="area" data-key="nōnne"></div> mē exspectat?”</div>
+      <div>4. Iūlia: “<div class="area" data-key="nōnne"></div> haec rosa pulchra est?”</div>
+      <div>5. Aemilia: “<div class="area" data-key="num"></div> hīc centum nummī sunt?”</div>
+      <div>6. Albīnus: “<div class="area" data-key="nōnne"></div> pulchrī sunt hī ānulī?”</div>
+      <div>7. Mēdus: “<div class="area" data-key="num"></div> ānulus sine gemmā vīgintī sēstertiīs cōnstat?”</div>
+      <div>8. <div class="area" data-key="num"></div> Hispānia īnsula est?</div>
+      <div>9. <div class="area" data-key="nōnne"></div> magnum est imperium Rōmānum?</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 6</b></div>
+      <div><br></div>
+      <div><i><div class="area">clāmāre</div> <div class="area">cōnstāre</div> <div class="area">convenīre</div> <div class="area">gemmātus</div> <div class="area">ille</div> <div class="area">mōnstrāre</div> <div class="area">octōgintā</div> <div class="area">ostendere</div> <div class="area">pretium</div> <div class="area">quam</div> <div class="area">sēstertius</div> <div class="area">tantus</div></i></div>
+      <div><br></div>
+      <div>1. Albīnus <div class="area" data-key="clāmat"></div>: “Ōr-nā-men-ta!”</div>
+      <div>2. Lȳdia: “Aspice <div class="area" data-key="illam"></div> tabernam, Mēde!”</div>
+      <div>3. Lȳdia tabernam Albīnī digitō <div class="area" data-key="mōnstrat"></div>.</div>
+      <div>4. Lȳdia: “Ō, <div class="area" data-key="quam"></div> pulchra sunt <div class="area" data-key="illa"></div> ōrnāmenta!”</div>
+      <div>5. Albīnus Lȳdiae et Mēdō ānulōs <div class="area" data-key="ostendit"></div>: “Aspicite hōs ānulōs!”</div>
+      <div>6. Mēdus: “Quot nummīs <div class="area" data-key="cōnstat"></div> ānulus <div class="area" data-key="gemmātus"></div>?”</div>
+      <div>7. Albīnus: “<div class="area" data-key="pretium"></div> huius ānulī est centum <div class="area" data-key="sēstertiī"></div>. Haec gemma sōla <div class="area" data-key="octōgintā | LXXX"></div> sēstertiīs cōnstat.</div>
+      <div>8. Mēdus: “<div class="area" data-key="tanta"></div> gemma ad tam parvum ānulum nōn <div class="area" data-key="convenit"></div>.”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III<br></div>
+      <div><br></div>
+      <div><b>Exercitium 8</b></div>
+      <div><br></div>
+      <div><i><div class="area">-it</div> <div class="area">-iunt</div> /</i> <i><div class="area">-e</div> <div class="area">-ite</div></i></div>
+      <div><br></div>
+      <div>1. Lȳdia: “Aspic<div class="area" data-key="e"></div> illam tabernam, Mēde!” Mēdus tabernam aspic<div class="area" data-key="it"></div>.</div>
+      <div>2. Mēdus et Lȳdia ōrnāmenta aspic<div class="area" data-key="iunt"></div>.</div>
+      <div>3. Albīnus iīs ānulum ostendit: “Aspic<div class="area" data-key="ite"></div> hunc ānulum!”</div>
+      <div>4. Iūlia Iūliō rosam ostendit: “Aspic<div class="area" data-key="e"></div> hanc rosam, pater!” Iūlius rosam aspic<div class="area" data-key="it"></div>.</div>
+      <div>5. Iūlia: “Accip<div class="area" data-key="e"></div> rosam, pater!” Pater rosam accip<div class="area" data-key="it"></div> ā fīliā suā.</div>
+      <div>6. Iūlius: “Accip<div class="area" data-key="ite"></div> māla, servī!” Servī māla accip<div class="area" data-key="iunt"></div> ā dominō.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 9</b></div>
+      <div><br></div>
+      <div><i><div class="area">aut</div> <div class="area">medius</div> <div class="area">nimis</div> <div class="area">nōnāgintā</div> <div class="area">quantus</div> <div class="area">quārtus</div> <div class="area">satis</div> <div class="area">tantus</div> <div class="area">vīgintī</div></i></div>
+      <div><br></div>
+      <div>1. <div class="area" data-key="quanta"></div> pecūnia est in sacculō Mēdī? <div class="area" data-key="nōnāgintā | XC"></div> sēstertiī.</div>
+      <div>2. Albīnus: “Id nōn <div class="area" data-key="satis"></div> est. Pretium ānulī est sēstertiī centum.”</div>
+      <div>3. Mēdus: “Id est <div class="area" data-key="nimis"></div> magnum pretium. Accipe nōnāgintā sēstertiōs <div class="area" data-key="aut"></div> nūllōs!”</div>
+      <div>4. Ānulus nōn in digitō <div class="area" data-key="mediō"></div>, sed in digitō <div class="area" data-key="quārtō"></div> pōnitur.</div>
+      <div>5. Digitus quārtus nōn <div class="area" data-key="tantus"></div> est <div class="area" data-key="quantus"></div> digitus medius.</div>
+      <div>6. Decem et decem sunt <div class="area" data-key="vīgintī | XX"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 10</b></div>
+      <div><br></div>
+      <div><i>ablātīvus pretiī</i></div>
+      <div><br></div>
+      <div>1. Mēdus: “Quot numm<div class="area" data-key="īs"></div> cōnstat ānulus gemmātus?”</div>
+      <div>2. Albīnus: “Hic ānulus centum sēsterti<div class="area" data-key="īs"></div> cōnstat.”</div>
+      <div>3. Mēdus: “Aliī ānulum gemmātum octōgintā sēsterti<div class="area" data-key="īs"></div> vēndunt.”</div>
+      <div>4. Quant<div class="area" data-key="ō"></div> preti<div class="area" data-key="ō"></div> Mēdus ānulum emit?</div>
+      <div>5. Mēdus ānulum emit sēsterti<div class="area" data-key="īs"></div> nōnāgintā.</div>
+      <div>6. Albīnus Mēdō ānulum vēndit sēsterti<div class="area" data-key="īs"></div> nōnāgintā.</div>
+      <div>7. Nōn magn<div class="area" data-key="ō"></div> preti<div class="area" data-key="ō"></div> Mēdus ānulum emit.</div>
+    </div>
+  `
+});
