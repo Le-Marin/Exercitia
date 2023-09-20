401459fe791e14e13e206d1a5a0714f0cd7e9372
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2023-09-20 16:15:51 +0300

    Add files

---
+++ src/communia/lectio/lec-03dpkys2.js
@@ -0,0 +1,47 @@
+$_GET({
+  title: 'COMMVNIA: Lectio 3',
+  keys: 'Á É Í Ó Ú Ý | Ā́ Ḗ Ī́ Ṓ Ū́ Ȳ́ Ǽ Œ́',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Разделить на слоги, проставить ударения, прочитать.</b></div>
+      <div><br></div>
+      <div><i>Exemplum</i>:<span style="white-space: pre;">	</span>cadāver <div class="area">труп</div> → cadāver <div class="area">ca-dā́-ver</div></div>
+      <div><br></div>
+      <div>maxilla <div class="area" data-key="ma-xíl-la">верхняя челюсть</div></div>
+      <div>calvaria <div class="area" data-key="cal-vá-ri-a">черепной свод</div></div>
+      <div>cementum <div class="area" data-key="ce-mén-tum">цемент</div></div>
+      <div>phthisis <div class="area" data-key="phthí-sis">туберкулёз, чаxотка</div></div>
+      <div>arthrītis <div class="area" data-key="ar-thrī́-tis">артрит</div></div>
+      <div>oedēma <div class="area" data-key="oe-dḗ-ma">отёк</div></div>
+      <div>oesophagus <div class="area" data-key="oe-só-pha-gus">пищевод</div></div>
+      <div>laryngēus <div class="area" data-key="la-ryn-gḗ-us">гортанный</div></div>
+      <div>osseus <div class="area" data-key="ós-se-us">костяной</div></div>
+      <div>pleura <div class="area" data-key="péu-ra">плевра</div></div>
+      <div>myologia <div class="area" data-key="my-o-ló-gi-a">миология</div></div>
+      <div>bronchioli <div class="area" data-key="bron-chí-o-li">бронxиолы</div></div>
+      <div>cicātrix <div class="area" data-key="ci-cā́-trix">шрам, рубец</div></div>
+      <div>sutūra <div class="area" data-key="su-tū́-ra">шов</div></div>
+      <div>antebrachium <div class="area" data-key="an-te-brá-chi-um">предплечье</div></div>
+      <div>tympanum <div class="area" data-key="tým-pa-num">барабан</div></div>
+      <div>uterus <div class="area" data-key="ú-te-rus">матка</div></div>
+      <div>urēter <div class="area" data-key="u-rḗ-ter">мочеточник</div></div>
+      <div>trachēa <div class="area" data-key="tra-chḗ-a">траxея</div></div>
+      <div>aegrōtus <div class="area" data-key="ae-grṓ-tus">больной</div></div>
+      <div>antidotum <div class="area" data-key="an-tí-do-tum">противоядие</div></div>
+      <div>liquor <div class="area" data-key="lí-quor">жидкость</div></div>
+      <div>solutio <div class="area" data-key="so-lú-ti-o">раствор</div></div>
+      <div>ampulla <div class="area" data-key="am-púl-la">ампула</div></div>
+      <div>hirūdo <div class="area" data-key="hi-rū́-do">пиявка</div></div>
+      <div>Sanguisorba <div class="area" data-key="San-gui-sór-ba">кровохлёбка (растение)</div></div>
+      <div>Hippophaë <div class="area" data-key="Hip-pó-pha-ë | Hip-pó-pha-e">облепиxа</div></div>
+      <div>Crataegus <div class="area" data-key="Cra-tǽ-gus">боярышник</div></div>
+      <div>Foeniculum <div class="area" data-key="Foe-ní-cu-lum">укроп, фенxель</div></div>
+      <div>Eucalyptus <div class="area" data-key="Eu-ca-lýp-tus">эвкалипт</div></div>
+      <div>Juniperus <div class="area" data-key="Ju-ní-pe-rus">можевельник</div></div>
+      <div>Myrtillus <div class="area" data-key="Myr-tíl-lus">черника</div></div>
+      <div>Quercus <div class="area" data-key="Quér-cus">дуб</div></div>
+      <div>Aurum <div class="area" data-key="Áu-rum">золото</div></div>
+      <div>Hydrargyrum <div class="area" data-key="Hy-drár-gy-rum">ртуть</div></div>
+    </div>
+  `
+});
---
+++ src/communia/lectio/lec-04ica8x6.js
@@ -0,0 +1,47 @@
+$_GET({
+  title: 'COMMVNIA: Lectio 4',
+  keys: 'Á É Í Ó Ú Ý | Ā́ Ḗ Ī́ Ṓ Ū́ Ȳ́ Ǽ Œ́',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Разделить на слоги, проставить ударения, прочитать.</b></div>
+      <div><br></div>
+      <div><i>Exemplum</i>:<span style="white-space: pre;">	</span>cadāver <div class="area">труп</div> → cadāver <div class="area">ca-dā́-ver</div></div>
+      <div><br></div>
+      <div>sanguis <div class="area" data-key="sán-guis">кровь</div></div>
+      <div>angulus <div class="area" data-key="an-gu-lus">угол</div></div>
+      <div>cholera <div class="area" data-key="chó-le-ra">холера</div></div>
+      <div>squama <div class="area" data-key="squá-ma">чешуя</div></div>
+      <div>intestīnum <div class="area" data-key="in-tes-tī́-num">кишка, кишечник</div></div>
+      <div>ischiaticus <div class="area" data-key="is-chi-á-ti-cus">седалищный</div></div>
+      <div>pharynx <div class="area" data-key="phá-rynx">глотка</div></div>
+      <div>cartilāgo <div class="area" data-key="car-ti-lā́-go">хрящ</div></div>
+      <div>aequālis <div class="area" data-key="ae-quā́-lis">равный</div></div>
+      <div>uterus <div class="area" data-key="ú-te-rus">матка</div></div>
+      <div>thyreoideus <div class="area" data-key="thy-re-o-í-de-us">щитовидный</div></div>
+      <div>bronchioli <div class="area" data-key="bron-chí-o-li">бронхиолы</div></div>
+      <div>skeleton <div class="area" data-key="ské-le-ton">скелет</div></div>
+      <div>hypoxīa <div class="area" data-key="hy-po-xī́-a">пониженное содержание кислорода</div></div>
+      <div>unguentum <div class="area" data-key="un-guén-tum">мазь</div></div>
+      <div>mixtio <div class="area" data-key="míx-ti-o">смесь</div></div>
+      <div>rhinītis <div class="area" data-key="rhi-nī́-tis">воспаление носа</div></div>
+      <div>thoracicus <div class="area" data-key="tho-rá-ci-cus">грудной</div></div>
+      <div>tibia <div class="area" data-key="tí-bi-a">малоберцовая кость</div></div>
+      <div>fibula <div class="area" data-key="fí-bu-la">большая берцовая кость</div></div>
+      <div>organum <div class="area" data-key="ór-ga-num">орган</div></div>
+      <div>omentum <div class="area" data-key="o-mén-tum">сальник</div></div>
+      <div>paediatrīa <div class="area" data-key="pae-di-a-trí-a">педиатрия</div></div>
+      <div>pathologia <div class="area" data-key="pa-tho-ló-gi-a">патология</div></div>
+      <div>ophthalmologus <div class="area" data-key="oph-thal-mó-lo-gus">офтальмолог</div></div>
+      <div>anterior <div class="area" data-key="an-té-ri-or">задний</div></div>
+      <div>cubitus <div class="area" data-key="cú-bi-tus">локоть</div></div>
+      <div>sanguineus <div class="area" data-key="san-guí-ne-us">кровяной</div></div>
+      <div>cellula <div class="area" data-key="cél-lu-la">клетка</div></div>
+      <div>apnoë <div class="area" data-key="áp-no-ë | áp-no-e">апноэ (временная остановка дыxания)</div></div>
+      <div>vertebra <div class="area" data-key="vér-te-bra">позвонок</div></div>
+      <div>Ephedra <div class="area" data-key="É-phe-dra">хвойник</div></div>
+      <div>Glycyrrhīza <div class="area" data-key="Gly-cyr-rhī́-za">солодка</div></div>
+      <div>Arachis <div class="area" data-key="Á-ra-chis">арахис</div></div>
+      <div>Saccharum <div class="area" data-key="Sác-cha-rum">сахар</div></div>
+    </div>
+  `
+});
