e0a392e6c2b5a5bd6840c0c08c1ae300a8b2323b
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2023-12-02 15:12:51 +0300

    Add files via upload

---
+++ src/llpsi/ultra/ultra-15cw7ur4.js
@@ -0,0 +1,26 @@
+$_GET({
+  title: 'LLPSI: Ultra 15',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести словосочетания с настоящими действительными причастиями.</b></div>
+      <div><br></div>
+      <div><div><i>Exemplum</i>: про слуг, боящихся хозяев <div class="area">dē servīs dominōs timentibus</div></div></div>
+      <div><br></div>
+      <div><div>1. вокруг стен, опоясывающих <i>(cingere)</i> город <div class="area" data-key="circum mūrōs oppidum cingentēs | circum mūrōs urbem cingentēs"></div></div>
+      <div>2. от границы, разделяющей две области <div class="area" data-key="ā fīne duās regiōnēs dīvidente"></div></div>
+      <div>3. между войсками, сражающимися в битве <div class="area" data-key="inter exercitūs in proeliō pugnantēs"></div></div>
+      <div>4. с другом, живущем далеко <div class="area" data-key="cum amīcō procul habitante"></div></div>
+      <div>5. без дыхания, дающего жизнь <div class="area" data-key="sine animā vītam dante"></div></div>
+      <div>6. под дерево, стоящее на холме <div class="area" data-key="sub arborem in colle stantem"></div></div>
+      <div>7. на дорогах, ведущих в Рим <div class="area" data-key="in viīs Rōmam dūcentibus"></div></div>
+      <div>8. у <i>(apud)</i> людей, скрывающих свои дела <i>(rēs)</i> <div class="area" data-key="apud hominēs rēs suās occultantēs"></div></div>
+      <div>9. по лицам, выражающим (≈ показывающим) страх <div class="area" data-key="per faciēs timōrem ostendentēs"></div></div>
+      <div>10. в глазах, глядящих на меня <div class="area" data-key="in oculīs mē aspicientibus"></div></div>
+      <div>11. за полководцем, зовущем к битве <div class="area" data-key="post ducem ad proelium vocantem"></div></div>
+      <div>12. с гор, устремляющихся в небо <div class="area" data-key="dē montibus caelum petentibus"></div></div>
+      <div>13. из города, лежащего у <i>(ad)</i> горы <div class="area" data-key="ex oppidō ad montem iacente"></div></div>
+      <div>14. под сердцем, гоняющем (≈ водящем) кровь <div class="area" data-key="sub corde sanguinem dūcente"></div></div>
+      <div>15. от брата, пишущего своей сестре <div class="area" data-key="ā frātre sorōrī suae scrībente"></div></div></div>
+    </div>
+  `
+});
