ab59878b13cd50473d109ea41bc7b59219355b39
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2023-12-02 15:01:05 +0300

    Add files via upload

---
+++ src/llpsi/ultra/ultra-14hft9pv.js
@@ -0,0 +1,29 @@
+$_GET({
+  title: 'LLPSI: Ultra 14',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести словосочетания с настоящими действительными причастиями.</b></div>
+      <div><br></div>
+      <div><div><i>Exemplum</i>:<span style="white-space: pre;">	</span><span style="white-space: pre;">	</span>про боящихся слуг <div class="area">dē servīs timentibus</div></div>
+      <div><span style="white-space: pre;">	</span><span style="white-space: pre;">	</span><span style="white-space: pre;">	</span><span style="white-space: pre;">	</span>про слуг, боящихся хозяев <div class="area">dē servīs dominōs timentibus</div></div></div>
+      <div><br></div>
+      <div>1. о сражающихся римлянах <div class="area" data-key="dē Rōmānīs pugnantibus"></div></div>
+      <div>2. к спящему пастуху <div class="area" data-key="ad pāstōrem dormientem"></div></div>
+      <div>3. под светящим солнцем <div class="area" data-key="sub sōle lūcente | sub sōle lūcentī"></div> </div>
+      <div>4. в поющем голосе <div class="area" data-key="in vōce cantante | in vōce cantantī"></div></div>
+      <div>5. из слушающих учеников <div class="area" data-key="ē discipulīs audientibus"></div></div>
+      <div>6. подле плачущей госпожи <div class="area" data-key="apud dominam plōrantem"></div></div>
+      <div>7. в летящих птиц <div class="area" data-key="in avēs volantēs"></div></div>
+      <div>8. по падающим звёздам <div class="area" data-key="per stēllās cadentēs"></div></div>
+      <div><br></div>
+      <div>1. на служанку, открывающую окно <div class="area" data-key="in ancillam fenestram aperientem"></div></div>
+      <div>2. у греков, рассказывающих истории <div class="area" data-key="apud Graecōs fābulās nārrantēs"></div></div>
+      <div>3. к матери, зовущей меня <div class="area" data-key="ad mātrem mē vocantem"></div></div>
+      <div>4. от зверей, населяющих леса <div class="area" data-key="ā bēstiīs silvās incolentibus"></div></div>
+      <div>5. без солдат, носящих дротики <div class="area" data-key="sine mīlitibus pīla gerentibus"></div></div>
+      <div>6. с коня, везущего Юлия <div class="area" data-key="dē equō Iūlium vehente"></div></div>
+      <div>7. около города, имеющего тысячу жителей <div class="area" data-key="prope oppidum centum incolās habēns | prope urbem centum incolās habentem"></div></div>
+      <div>8. в человеке, стоящим перед нами <div class="area" data-key="in homine ante nōs stante"></div></div>
+    </div>
+  `
+});
