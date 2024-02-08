c4cedd28a80b9f995e6f435febcd60881f4ecb2d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-02-08 21:42:50 +0300

    Add files via upload

---
+++ src/llpsi/ultra/ultra-16itxodb.js
@@ -0,0 +1,52 @@
+$_GET({
+  title: 'Ultra 16',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести словосочетания с отложительными глаголами </b><i>(глаголы ставить в конце фразы)</i><b>.</b></div>
+      <div><br></div>
+      <div>1. всматривается в море <div class="area" data-key="mare intuētur"></div></div>
+      <div>2. опасаетесь этого человека <div class="area" data-key="hunc hominem verēminī"></div></div>
+      <div>3. обнимаю любимую женщину <div class="area" data-key="fēminam amātam complector"></div></div>
+      <div>4. утешаем больных <div class="area" data-key="aegrōtōs cōnsōlāmur"></div></div>
+      <div>5. дожидаешься своего друга <div class="area" data-key="amīcum tuum opperīris"></div></div>
+      <div>6. следуют за военным кораблём <i>(nāvis longa)</i> <div class="area" data-key="nāvem longam sequuntur"></div></div>
+      <div>7. рассматриваете её украшения <div class="area" data-key="ōrnāmenta eius intuēminī"></div></div>
+      <div>8. страшусь гнева <i>(īra)</i> богов <div class="area" data-key="īram deōrum vereor"></div></div>
+      <div>9. обнимает своего брата <div class="area" data-key="frātrem suum complectitur"></div></div>
+      <div>10. обхватываешь шею руками <div class="area" data-key="collum manibus complecteris"></div></div>
+      <div><br></div>
+      <div>1. радуемся красивым вещам <i>(abl.)</i> <div class="area" data-key="rēbus pulchrīs laetāmur"></div></div>
+      <div>2. солнце встает над морем <div class="area" data-key="sōl suprā mare oritur"></div></div>
+      <div>3. солдаты выступают из лагеря <div class="area" data-key="mīlitēs ex castrīs ēgrediuntur"></div></div>
+      <div>4. разговариваете с полководцем <div class="area" data-key="cum duce loquiminī"></div></div>
+      <div>5. войско отправляется утром <div class="area" data-key="exercitus māne proficīscitur"></div> </div>
+      <div>6. слёзы скатываются по щекам <div class="area" data-key="lacrimae per genās lābuntur"></div></div>
+      <div>7. пытаюсь управлять кораблём <div class="area" data-key="nāvem gubernāre cōnor"></div></div>
+      <div>8. в сердце поднимается буря <div class="area" data-key="in corde tempestās oritur"></div></div>
+      <div>9. моряки выходят из гавани <div class="area" data-key="nautae ē portū ēgrediuntur"></div></div>
+      <div>10. одежда спадает с плеч <div class="area" data-key="vestīmentum dē umerīs lābitur"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>2. Перевести словосочетания с настоящими действительными причастиями от отложительных глаголов</b> <i>(причастия ставить в конце фразы)</i>.</div>
+      <div><br></div>
+      <div><i>Exemplum</i>: про слуг, опасающихся хозяев <div class="area">dē servīs dominōs verentibus</div></div>
+      <div><br></div>
+      <div>1. рулевой, дожидающийся попутного ветра <div class="area" data-key="gubernātor ventum secundum opperiēns"></div></div>
+      <div>2. люди, выходящие из города <div class="area" data-key="hominēs ex urbe ēgredientēs"></div></div>
+      <div>3. о солнце, восходящем на небе <div class="area" data-key="dē sōle in caelō oriente"></div></div>
+      <div>4. вестником, отправляющимся в лагерь <div class="area" data-key="ā nūntiō in castra proficīscente"></div></div>
+      <div>5. среди солдат, следующих за полководцем <div class="area" data-key="inter mīlitēs ducem sequentēs"></div></div>
+      <div>6. для <i>(prō)</i> рабов, радующихся милости <i>(grātia, abl.)</i> <div class="area" data-key="prō servīs grātiā laetantibus"></div></div>
+      <div>7. от больной, опасающейся смерти <div class="area" data-key="ab aegrōtā mortem verentem"></div></div>
+      <div>8. на Цезаря, всматривающегося в лица сенаторов <div class="area" data-key="in Caesarem faciēs senātōrum intuentem"></div></div>
+      <div>9. без учителя, говорящего по-латыни <i>(Latīnē)</i> <div class="area" data-key="sine magistrō Latīnē loquente"></div></div>
+      <div>10. с войском, пытающимся защищать вал <div class="area" data-key="cum exercitū vāllum dēfendere cōnante"></div></div>
+      <div>11. матерям, утешающим своих сыновей <div class="area" data-key="mātribus fīliōs suōs cōnsōlantibus"></div></div>
+      <div>12. во льду, обхватывающем землю <div class="area" data-key="in glaciē terram complectente"></div></div>
+      <div>13. со слезами, катящимися из глаз <div class="area" data-key="cum lacrimīs ex oculīs lābentibus"></div></div>
+      <div>14. у древних, страшащихся природы <div class="area" data-key="apud antīquōs nātūram verentēs"></div></div>
+      <div>15. к послам <i>(lēgātus)</i>, выезжающим из Рима <div class="area" data-key="ad lēgātōs Rōmā ēgredientēs"></div></div>
+    </div>
+  `
+});
